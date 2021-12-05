c047db7926a5c4023b833e2492fa774a683fe404
Jake Swenson <jake@jakeswenson.com>
2021-12-05 09:42:23 -0800

    style(day05): Clean up Day05

--- adventcli/adventcli/days/day05.swift
+++ adventcli/adventcli/days/day05.swift
@@ -1,44 +1,36 @@
+import Algorithms
+import Collections
 import Foundation
 import Parsing
-import Collections
-import Algorithms
 
 struct Point: Equatable, Hashable {
-    let x: UInt16, y:UInt16
+    let x: UInt16, y: UInt16
 }
 
 struct Line {
     let start: Point, end: Point
-    
-    func intersectOrthoganlly(other: Line) -> Bool {
-        if other.start == start || other.start == end
-            || other.end == start || other.end == end{
-            return true
-        }
-        
-        
-        return false
-    }
-    
+
+    var isHorizontal: Bool { start.y == end.y }
+    var isVertical: Bool { start.x == end.x }
+
     func pointsOnLine() -> [Point] {
-        
-        if start.x == end.x {
-            return (min(start.y, end.y)...max(start.y, end.y)).map { Point(x: start.x, y: $0) }
+        if isVertical {
+            let (minY, maxY) = [start.y, end.y].minAndMax()!
+            return (minY ... maxY).map { Point(x: start.x, y: $0) }
+        } else if isHorizontal {
+            let (minX, maxX) = [start.x, end.x].minAndMax()!
+            return (minX ... maxX).map { Point(x: $0, y: start.y) }
         }
-        else if start.y == end.y {
-            return (min(start.x, end.x)...max(start.x, end.x)).map { Point(x: $0, y: start.y) }
-        }
-        
+
         let xs = stride(from: start.x, through: end.x, by: end.x < start.x ? -1 : 1)
         let ys = stride(from: start.y, through: end.y, by: end.y < start.y ? -1 : 1)
-        
+
         return zip(xs, ys).map { Point(x: $0, y: $1) }
-        
     }
 }
 
 let parsePoint = UInt16.parser().skip(",".utf8).take(UInt16.parser())
-    .map { x, y in Point(x: x, y: y)}
+    .map { x, y in Point(x: x, y: y) }
 
 let lineParser = parsePoint.skip(Whitespace()).skip("->".utf8).skip(Whitespace()).take(parsePoint)
     .map { start, end in Line(start: start, end: end) }
@@ -47,34 +39,34 @@ let lines = Many(lineParser, separator: Whitespace())
 
 let day05 = problem(day: 5) { text in
     let allLines = lines.parse(text.utf8) ?? []
-    
+
     part1 {
         print("Parsed Lines", allLines.count)
-        
-        let part1Lines = allLines.filter { line in
-            line.start.x == line.end.x || line.start.y == line.end.y
-        }
-        
+
+        let part1Lines = allLines.filter { line in line.isHorizontal || line.isVertical }
+
         print("Part1 Lines", part1Lines.count)
-        
-        var counts: [Point: UInt] = [:]
+
+        var counts: [Point: UInt8] = [:]
+
         for p in part1Lines.flatMap({ $0.pointsOnLine() }) {
             counts[p, default: 0] += 1
         }
-        
+
         let intersections = counts.values.filter { $0 >= 2 }.count
-        
+
         return intersections
     }
-    
+
     part2 {
-        var counts: [Point: UInt] = [:]
+        var counts: [Point: UInt8] = [:]
+
         for p in allLines.flatMap({ $0.pointsOnLine() }) {
             counts[p, default: 0] += 1
         }
-        
+
         let intersections = counts.values.filter { $0 >= 2 }.count
-        
+
         return intersections
     }
 }
