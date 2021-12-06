a7bf498df423b0e4d7f628ffcd62ddd4bedfc4fb
Jake Swenson <jake@jakeswenson.com>
2021-12-05 22:11:44 -0800

    feat(day06/2): Working part 2

--- adventcli/adventcli/days/day06.swift
+++ adventcli/adventcli/days/day06.swift
@@ -13,29 +13,27 @@ let manyFish = Many(parseFish, separator: ",")
 
 let day06 = problem(day: 6) { text in
     part1 {
-        var allFish = manyFish.parse(text)!
         
-        let fish = (0..<80).reduce(allFish) { currentFish, day in
-            var currentFish = currentFish
-            // print(currentFish)
-            
-            var newFish: [UInt8] = []
-            for idx in (0..<currentFish.count) {
-                if currentFish[idx] == 0 {
-                    currentFish[idx] = 7
-                    newFish.append(8)
-                }
-                currentFish[idx] -= 1
-                let value = currentFish[idx]
-                
-            }
-            
-            currentFish.append(contentsOf: newFish)
-            
-            return currentFish
+        let allFish = manyFish.parse(text)!
+        
+        var currentFish: [UInt64] = Array(repeating: 0, count: 9)
+        
+        for f in allFish {
+            currentFish[Int(f)] += 1
         }
         
-        return fish.count
+        for day in (0..<80) {
+            let newFishCount = currentFish[0]
+            for age in (1..<currentFish.count) {
+                currentFish[age-1] = currentFish[age]
+            }
+            currentFish[6] += newFishCount
+            currentFish[currentFish.count-1] = newFishCount
+        }
+        
+        let result = currentFish.reduce(0, +)
+        
+        return Int(result)
     }
     
     part2 {
@@ -47,7 +45,7 @@ let day06 = problem(day: 6) { text in
             currentFish[Int(f)] += 1
         }
         
-        for day in (0..<80) {
+        for day in (0..<256) {
             let newFishCount = currentFish[0]
             for age in (1..<currentFish.count) {
                 currentFish[age-1] = currentFish[age]
