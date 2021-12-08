82eae395fa2b319c81ad18278a85f7db4cab4440
Jake Swenson <jake@jakeswenson.com>
2021-12-07 23:39:36 -0800

    style(day8): Cleanup

--- adventcli/adventcli/days/day08.swift
+++ adventcli/adventcli/days/day08.swift
@@ -16,32 +16,6 @@ let numberSizes = [
     9: 6
 ]
 
-let numbers = [
-    0: "abcefg",
-    1: "cf",
-    2: "acdeg",
-    3: "acdfg",
-    4: "bcdf",
-    5: "abdfg",
-    6: "abdefg",
-    7: "acf",
-    8: "abcdefg",
-    9: "abcdfg"
-]
-
-let lookup = [
-    "abcefg"  : 0,
-    "cf"      : 1,
-    "acdeg"   : 2,
-    "acdfg"   : 3,
-    "bcdf"    : 4,
-    "abdfg"   : 5,
-    "abdefg"  : 6,
-    "acf"     : 7,
-    "abcdefg" : 8,
-    "abcdfg"  : 9
-]
-
 let day08 = problem(day: 8) { text in
     let notes: [(signals: [String.SubSequence], outputs: [String.SubSequence])] = text.split(whereSeparator: \.isNewline)
         .map { line in
@@ -67,6 +41,16 @@ let day08 = problem(day: 8) { text in
 
     part2 {
         let numberLengths: [Int] = ([1, 4, 7, 8].map { numberSizes[$0]! })
+
+        func matchDigit(_ chars: [Set<Character>], by: (Set<Character>) -> Bool) -> (Set<Character>, Array<Set<Character>>.SubSequence) {
+            var chars = chars
+            let partitionIdx = chars.partition { !by($0) }
+
+            let first = chars[..<partitionIdx]
+            assert(first.count == 1)
+            return (first[0], chars[partitionIdx...])
+        }
+
         let result = notes.map { note in 
 
             let signalMap = Dictionary(uniqueKeysWithValues: note.signals
@@ -82,22 +66,15 @@ let day08 = problem(day: 8) { text in
             let upperL = four.subtracting(one)
             let top = seven.subtracting(one)
 
-            let unknownSignals = note.signals.filter { signal in
-                !known.contains {
-                    $0 == Set(signal)
-                }
-            }.map { Set($0) }
+            let unknownSignals = note.signals.map{ Set($0) }.filter { signal in
+                !known.contains { k in k == signal }
+            }
 
             let nineMatcher = upperL.union(top).union(one)
-            
-            let nine = unknownSignals.first { sig in
+
+            let (nine, remaining) = matchDigit(unknownSignals) { sig in 
                 nineMatcher.isStrictSubset(of: sig)
-            }!
-           
-            let bottom = nine.subtracting(nineMatcher)
-
-
-            let remaining = unknownSignals.filter { nine != $0 }
+            }
 
             let zeroOrThree = remaining.filter { seven.isStrictSubset(of: $0) }
 
@@ -114,38 +91,36 @@ let day08 = problem(day: 8) { text in
 
             let twoFiveSix = remaining.filter { zero != $0 && three != $0 }
 
-
-            let two = twoFiveSix.first {
-                upperL.subtracting($0).count == 1
-            }!
-
-            let fiveSix = twoFiveSix.filter { two != $0 }
-
-            let six = fiveSix.first {
-                eight.subtracting($0).count == 1
+            let (two, fiveSix) = matchDigit(twoFiveSix) { option in
+                upperL.subtracting(option).count == 1
             }
 
-            let five = fiveSix.first {
-                eight.subtracting($0).count == 2
+            let six = fiveSix.first { option in 
+                eight.subtracting(option).count == 1
             }
 
-            let number = note.outputs.map {
-                let output = Set($0)
-                if zero == output { return "0" }
-                if one == output { return "1" }
-                if two == output { return "2" }
-                if three == output { return "3" }
-                if four == output { return "4" }
-                if five == output { return "5" }
-                if six == output { return "6" }
-                if seven == output { return "7" }
-                if eight == output { return "8" }
-                return "9"
-            }.joined(separator: "")
+            let five = fiveSix.first { option in 
+                eight.subtracting(option).count == 2
+            }
 
-            let result = Int(number)!
+            func digit(_ output: Set<Character>) -> Int {
+                if zero == output { return 0 }
+                if one == output { return 1 }
+                if two == output { return 2 }
+                if three == output { return 3 }
+                if four == output { return 4 }
+                if five == output { return 5 }
+                if six == output { return 6 }
+                if seven == output { return 7 }
+                if eight == output { return 8 }
+                return 9
+            }
 
-            return result
+            let number = note.outputs.map { Set($0) }.reduce(0) { state, output in 
+                (10*state) + digit(output)
+            }
+
+            return number
         }.reduce(0, +)
 
         return result
