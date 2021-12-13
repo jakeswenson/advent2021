a93b164ccb6b2eab6c48c8b1a02642bcfff00ec5
Jake Swenson <jake@jakeswenson.com>
2021-12-12 21:57:51 -0800

    feat(day13): Finished

--- adventcli/adventcli/main.swift
+++ adventcli/adventcli/main.swift
@@ -2,7 +2,8 @@ import ArgumentParser
 import Foundation
 
 let days = [
-  day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12,
+  day01, day02, day03, day04, day05, day06, day07,
+  day08, day09, day10, day11, day12, day13
 ]
 
 struct Advent: ParsableCommand {
