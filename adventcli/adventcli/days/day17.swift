a0f90efacfc72984006f0738c0ca438235eb811c
Jake Swenson <jake@jakeswenson.com>
2021-12-18 17:42:02 -0800

    feat(day17): Day 17

--- adventcli/adventcli/days/day17.swift
+++ adventcli/adventcli/days/day17.swift
@@ -1,7 +1,57 @@
-import Foundation
+import Collections
+import Algorithms
+import Parsing
 
-let day17 = problem(day: 17) { _ in
-  part1 {
-    0
+
+let ws = Whitespace()
+
+let parseRange = Int.parser().skip("..".utf8).take(Int.parser()).map { $0...$1 }
+let day17Parser = Skip("target area:".utf8).skip(ws).skip("x=".utf8)
+  .take(parseRange)
+  .skip(",".utf8).skip(ws).skip("y=".utf8)
+  .take(parseRange)
+
+func maxDistance(_ dist: Int) -> Int {
+  ((dist+1)*dist)/2
+}
+
+let day17 = problem(day: 17) { input in
+  let (xRange, yRange) = day17Parser.parse(input.utf8)!
+  let minY = yRange.min()!
+  let maxY = maxDistance(minY)
+
+  part1(example: 45, answer: 30628) {
+    return maxY
+  }
+
+
+  part2(example: 112, answer: 30628) {
+    return (1...xRange.upperBound).reduce(Set<Coord>()) { results, initX in
+      if maxDistance(initX) < xRange.lowerBound {
+        return results
+      }
+
+      return (yRange.lowerBound...maxY).reduce(results) { results, initY in
+        var results = results
+        var xVelocity = initX, yVelocity = initY
+        var xPos = 0, yPos = 0
+
+        while xPos <= xRange.upperBound && yPos >= yRange.lowerBound {
+          if xPos >= xRange.lowerBound && yPos <= yRange.upperBound {
+            results.insert(Coord(initX, initY))
+            break
+          }
+
+          xPos += xVelocity
+          yPos += yVelocity
+
+          yVelocity-=1
+          xVelocity = max(xVelocity-1, 0)
+        }
+
+
+        return results
+      }
+    }.count
   }
 }
--- adventcli/adventcli/main.swift
+++ adventcli/adventcli/main.swift
@@ -4,7 +4,7 @@ import Foundation
 let days = [
   day01, day02, day03, day04, day05, day06, day07,
   day08, day09, day10, day11, day12, day13, day14,
-  day15, day16,
+  day15, day16, day17,
 ]
 
 struct Advent: ParsableCommand {
